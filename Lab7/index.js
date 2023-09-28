deb465ec89e5cc810150dbe3e6b6780786bccd8f
Valleyman <croccifixiodelena237@gmail.com>
2023-09-28 22:23:12 +0700

    Lab7 Done

--- Lab7/index.js
+++ Lab7/index.js
@@ -1,20 +1,29 @@
 let input = document.getElementById('num');
 let squareB = document.getElementById('squareB');
+let circleB = document.getElementById('circleB');
+let triangleB = document.getElementById('triangleB');
 let canvas = document.getElementById('canvas');
 
-squareB.addEventListener('click',function() {SquareInit(input.value)})
-
-function SquareInit(num){
+squareB.addEventListener('click',function() {FigureInit(input.value,'square')})
+circleB.addEventListener('click',function() {FigureInit(input.value,'circle')})
+triangleB.addEventListener('click',function() {FigureInit(input.value,'triangle')})
+function FigureInit(num,figure){
     for(let i=0;i < num;i++)
         {   
+
             console.log(num);
-            let newSquare = document.createElement('div');
-            let side = Math.random()*100;
-            let x = Math.random() * (canvas.clientWidth + side); // Random x-coordinate
-            let y = Math.random() * (canvas.clientHeight + side); // Random y-coordinate 
-            newSquare.style= `width:${side}px;height: ${side}px;top: ${x}px;left:${y}px`;
-            console.log(`${newSquare.style.width}`,x,y);
-            newSquare.classList.add('square');
-            canvas.appendChild(newSquare);
+            let newFigure = document.createElement('div');
+            let side = Math.random()*150;
+            let x = 60 + Math.random() * 350; // Random x-coordinate
+            let y = 60 + Math.random() * 350 +Math.random() * 150 + Math.random() * 150; // Random y-coordinate 
+            newFigure.classList.add(`${figure}`);                                        //Поставить фигуру
+            newFigure.addEventListener('dblclick',()=>{newFigure.remove()})              //Удалить при 2-разном нажитии
+            console.log(figure);
+            if (figure=='triangle'){
+                newFigure.style= `border-bottom-width:${side}px;border-left-width:${side}px;border-right-width:${side}px;top: ${x}px;left:${y}px`;
+            }
+            else{newFigure.style= `width:${side}px;height: ${side}px;top: ${x}px;left:${y}px`;}
+            console.log(`${newFigure.style.width}`,x,y);
+            canvas.appendChild(newFigure);
         }
 }
