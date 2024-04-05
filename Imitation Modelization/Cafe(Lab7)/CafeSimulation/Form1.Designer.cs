76693a733fca4e507bec30f7413ae7ff5b1153ed
Valleyman <croccifixiodelena237@gmail.com>
2024-04-06 02:14:41 +0700

    lab7

--- Imitation Modelization/Cafe(Lab7)/CafeSimulation/Form1.Designer.cs
+++ Imitation Modelization/Cafe(Lab7)/CafeSimulation/Form1.Designer.cs
@@ -29,12 +29,237 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.marketing = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label2 = new System.Windows.Forms.Label();
+            this.inventory = new System.Windows.Forms.NumericUpDown();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.competition = new System.Windows.Forms.NumericUpDown();
+            this.pricinginput = new System.Windows.Forms.NumericUpDown();
+            this.skill = new System.Windows.Forms.NumericUpDown();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.marketing)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.inventory)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.competition)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pricinginput)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.skill)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.skill);
+            this.panel1.Controls.Add(this.pricinginput);
+            this.panel1.Controls.Add(this.competition);
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.inventory);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.marketing);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Location = new System.Drawing.Point(1, 1);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(807, 130);
+            this.panel1.TabIndex = 0;
+            // 
+            // marketing
+            // 
+            this.marketing.DecimalPlaces = 1;
+            this.marketing.Increment = new decimal(new int[] {
+            1,
+            0,
+            0,
+            65536});
+            this.marketing.Location = new System.Drawing.Point(134, 30);
+            this.marketing.Name = "marketing";
+            this.marketing.Size = new System.Drawing.Size(61, 20);
+            this.marketing.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(27, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(97, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Marketing expense";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(589, 32);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(179, 70);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // chart1
+            // 
+            chartArea1.AxisX.TitleForeColor = System.Drawing.Color.IndianRed;
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(12, 137);
+            this.chart1.Name = "chart1";
+            series1.BorderWidth = 2;
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series1.Legend = "Legend1";
+            series1.Name = "Cost";
+            series2.BorderWidth = 2;
+            series2.ChartArea = "ChartArea1";
+            series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series2.Legend = "Legend1";
+            series2.Name = "Revenue";
+            this.chart1.Series.Add(series1);
+            this.chart1.Series.Add(series2);
+            this.chart1.Size = new System.Drawing.Size(793, 483);
+            this.chart1.TabIndex = 1;
+            this.chart1.Text = "chart1";
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(27, 61);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(94, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Inventory expense";
+            // 
+            // inventory
+            // 
+            this.inventory.DecimalPlaces = 1;
+            this.inventory.Increment = new decimal(new int[] {
+            1,
+            0,
+            0,
+            65536});
+            this.inventory.Location = new System.Drawing.Point(134, 61);
+            this.inventory.Name = "inventory";
+            this.inventory.Size = new System.Drawing.Size(61, 20);
+            this.inventory.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(240, 32);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(97, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Market competition";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(240, 63);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(39, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Pricing";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(240, 89);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(73, 13);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Employee skill";
+            // 
+            // competition
+            // 
+            this.competition.DecimalPlaces = 1;
+            this.competition.Increment = new decimal(new int[] {
+            1,
+            0,
+            0,
+            65536});
+            this.competition.Location = new System.Drawing.Point(368, 30);
+            this.competition.Name = "competition";
+            this.competition.Size = new System.Drawing.Size(61, 20);
+            this.competition.TabIndex = 10;
+            // 
+            // pricinginput
+            // 
+            this.pricinginput.DecimalPlaces = 1;
+            this.pricinginput.Increment = new decimal(new int[] {
+            1,
+            0,
+            0,
+            65536});
+            this.pricinginput.Location = new System.Drawing.Point(368, 59);
+            this.pricinginput.Name = "pricinginput";
+            this.pricinginput.Size = new System.Drawing.Size(61, 20);
+            this.pricinginput.TabIndex = 11;
+            // 
+            // skill
+            // 
+            this.skill.DecimalPlaces = 1;
+            this.skill.Increment = new decimal(new int[] {
+            1,
+            0,
+            0,
+            65536});
+            this.skill.Location = new System.Drawing.Point(368, 85);
+            this.skill.Name = "skill";
+            this.skill.Size = new System.Drawing.Size(61, 20);
+            this.skill.TabIndex = 12;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(806, 622);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Cafe";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.marketing)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.inventory)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.competition)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pricinginput)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.skill)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.NumericUpDown marketing;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.NumericUpDown inventory;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.NumericUpDown skill;
+        private System.Windows.Forms.NumericUpDown pricinginput;
+        private System.Windows.Forms.NumericUpDown competition;
+        private System.Windows.Forms.Label label5;
     }
 }
 
--- Imitation Modelization/Cafe(Lab7)/CafeSimulation/Form1.cs
+++ Imitation Modelization/Cafe(Lab7)/CafeSimulation/Form1.cs
@@ -16,5 +16,91 @@ namespace CafeSimulation
         {
             InitializeComponent();
         }
+        //константы
+        double marketingeffort = 0;
+        double inventorylevel = 0;
+        double marketcompetition = 0;
+        double pricing = 0;
+        double employeeSkill = 0;
+        int day = 0;
+        bool start = false;
+        //переменные
+        double reputation() { return marketingeffort + pricing - marketcompetition;}
+        double customerSatisfaction() {return taskDoneRate() +menuvarity(); }
+        double customerArrival() { return reputation()+customerSatisfaction(); }
+        double preprationEffort() { return employee * 0.1; }
+        double menuvarity() { return inventorylevel; }
+        //stock
+        double revenue = 1;
+        double cost = 1;
+        double profit= 1;
+        double employee = 10;
+        double task = 1;
+        double order = 1;
+        //vavle
+        double hiringRate() { return quitRate(); }
+        double quitRate() { return newTaskRate(); }
+        double newTaskRate() { return newOrderRate()-cancelOrderRate(); }
+        double taskDoneRate() { return preprationEffort() + employeeSkill; }
+        double cancelOrderRate() { if (customerSatisfaction() < 0) return -0.1 * customerSatisfaction();
+            else return 0; }
+        double newOrderRate() { return customerArrival() - pricing; }
+        double incomeRate() { return newOrderRate() + pricing; }
+        double costRate() { return marketingeffort+inventorylevel+hiringRate(); }
+        double profitRate() { return -0.0000001*incomeRate(); }
+        //evaluate stock after 1 day
+        public void evaluateTask() {
+            task = task + newTaskRate() - taskDoneRate();
+        }
+        public void evaluateCost() {
+            //cost = cost + costRate() - profitRate();
+            cost = revenue*costRate();
+        }
+        public void evaluateEmployee() {
+            employee = employee + hiringRate()-quitRate();
+        }
+        public void evaluateOrder() {
+            order = order + newOrderRate() - cancelOrderRate();
+        }
+        public void evaluateRevenue() {
+            revenue = revenue + incomeRate() - costRate();
+        }
+        public void evaluateProfit() {
+            profit = cost *(1-profitRate());
+        }
+        //evaluate variables
+  
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (!start)
+            {
+                marketingeffort = (double)marketing.Value;
+                inventorylevel = (double)inventory.Value;
+                marketcompetition = (double)competition.Value*10;
+                pricing = (double)pricinginput.Value;
+                employeeSkill = (double)skill.Value;
+                start = true;
+                timer1.Start();
+            }
+            else { start = false; timer1.Stop(); }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
+            calculateStep();
+            chart1.Series[0].Points.AddXY(day, cost);
+            chart1.Series[1].Points.AddXY(day, revenue);
+            day++;
+        }
+        public void calculateStep()
+        {
+            evaluateRevenue();
+            evaluateEmployee();
+            evaluateOrder();
+            evaluateTask();
+            evaluateCost();
+            //evaluateProfit();
+        }
     }
 }
